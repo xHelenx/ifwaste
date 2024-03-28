ae75ff87f2fb946c0881322bc6da2aa22125cae8
aco120 <helen.haase@haw-hamburg.de>
2024-03-28 12:41:52 -0400

    add manage global parameters in one script

---
+++ model/globalValues.py
@@ -0,0 +1,14 @@
+FTMEAT = "Meat & Fish" 
+FTDAIRY = "Dairy & Eggs" 
+FTVEGETABLE = "Fruits & Vegetables" 
+FTDRYFOOD = "Dry Foods & Baked Goods" 
+FTSNACKS = "Snacks, Condiments, Liquids, Oils, Grease, & Other" 
+FTSTOREPREPARED ='Store-Prepared Items' 
+
+
+EXPIRATION_THRESHOLD = 4
+MIN_TIME_TO_COOK = 0.8 #at least 30min to make a meal with 5 ingredients 
+MIN_TILL_SHOP = 5
+SERVINGS_PER_GRAB = 8
+INGREDIENTS_PER_QUICKCOOK = 2
+SCALER_SHOPPING_AMOUNT = 5
