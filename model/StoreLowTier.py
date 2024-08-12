9adf35594d2a5518ad6bceb4688c204dcf1ba66e
aco120 <helen.haase@haw-hamburg.de>
2024-08-12 14:02:52 -0400

    move parameterization to config

--- model/StoreLowTier.py
+++ model/StoreLowTier.py
@@ -14,8 +14,8 @@ class StoreLowTier(Store):
     def __init__(self, store_type:EnumStoreTier, grid:Grid, id) -> None:
         super().__init__(store_type, grid,id)
         
-        self.quality:float = 0.5 #todo
-        self.price:float = 0.5 #TODO calc based on stock avg p serving?
+        self.quality:float = globals.STORE_LOW_QUALITY 
+        self.price:float = globals.STORE_LOW_PRICE #TODO calc based on stock avg p serving?
         # high price is expensive, low is cheap -> 1-price in formulas
         
         self.buy_stock(amount_per_item=100)
