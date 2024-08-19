c27c8acb5d427f51431534812b9045d82a65ec7f
aco120 <helen.haase@haw-hamburg.de>
2024-08-19 10:15:57 -0400

    update Storage, make compatible with Df update

--- model/Storage.py
+++ model/Storage.py
@@ -1,30 +1,73 @@
 import pandas as pd
+import numpy as np
 
-from Food import Food
 from FoodGroups import FoodGroups
-
+import globals 
 class Storage: 
 
     def __init__(self) -> None:
         self.current_items:pd.DataFrame = pd.DataFrame(columns= [
-            'type', 
+            globals.FGMEAT, 
+            globals.FGDAIRY,
+            globals.FGVEGETABLE, 
+            globals.FGDRYFOOD,
+            globals.FGBAKED,
+            globals.FGSNACKS, 
+            globals.FGSTOREPREPARED,
+            'price',
+            'status',
             'servings', 
             'days_till_expiry'])
         self.fg:FoodGroups = FoodGroups.get_instance() # type: ignore
     
-    def add(self, item:Food) -> None: 
-        new_row = {"type" : item.type, "servings": item.servings, "days_till_expiry":item.days_till_expiry}
-        self.current_items.loc[len(self.current_items)] = new_row # type: ignore
+    def add(self, item:pd.Series) -> None: 
+        self.current_items.loc[len(self.current_items)] = item
         self.current_items.reset_index()
         
     
-    def remove(self,item:Food) -> None:
-        row_to_remove = self.current_items[(self.current_items["type"]==item.type) &\
-            (self.current_items["servings"] == item.servings) &\
+    def remove(self,item:pd.Series) -> None:
+        row_to_remove = self.current_items[
+            (self.current_items[globals.FGMEAT] == item[globals.FGMEAT] ) &
+            (self.current_items[globals.FGDAIRY] == item[globals.FGDAIRY] ) &
+            (self.current_items[globals.FGVEGETABLE] == item[globals.FGVEGETABLE] ) &
+            (self.current_items[globals.FGDRYFOOD] == item[globals.FGDRYFOOD] ) &
+            (self.current_items[globals.FGBAKED] == item[globals.FGBAKED] ) &
+            (self.current_items[globals.FGSNACKS] == item[globals.FGSNACKS] ) &
+            (self.current_items[globals.FGSTOREPREPARED] == item[globals.FGSTOREPREPARED]) &
+            (self.current_items["price"] == item.price) &
+            (self.current_items["status"] == item.status) &
+            (self.current_items["servings"] == item.servings) &
             (self.current_items["days_till_expiry"] == item.days_till_expiry)]
      
         self.current_items = self.current_items.drop(row_to_remove.index[0]) #just remove one row in case there are multiple
         
+    def get_item_by_strategy(self, strategy:str,preference_vector:dict[str,float]) -> pd.Series: 
+       #default random
+       #random" = choose random ingredients to cook wit
+       #"EEF"    = choose "Earliest Expiration First" 
+       
+        assert not self.is_empty()
+        
+        
+        if strategy == None or strategy == "random": 
+            #random grab 
+            fgs = list(preference_vector.keys())
+            weights = self.current_items[fgs].dot(pd.Series(preference_vector))
+            weights = weights / weights.sum()
+            weights = weights.astype(np.float64)
+            #weighted sample
+            idx = np.random.choice(self.current_items.index, size=1, replace=False, p=weights.values)
+            item = self.current_items.loc[idx].iloc[0]
+            
+        else: #EEF
+            idx = self.current_items['days_till_expiry'].idxmin()
+            item = self.current_items.loc[idx]
+            
+        self.remove(item) # type: ignore
+        return item      # type: ignore
+    
+   # def split_item()
+    
     def get_number_of_items(self) -> int:
         return len(self.current_items)
     
@@ -38,38 +81,13 @@ class Storage:
         fgs = self.fg.get_all_food_groups()
         result = pd.Series(dict(zip(fgs, [0]*len(fgs))))
         
-        for fg in fgs: 
-            servings_fg = self.current_items[self.current_items["type"] ==fg]["servings"].sum()
-            if servings_fg != None: 
-                result[fg] = 0
-            else:
-                result[fg] = servings_fg
+        return self.current_items[fgs].sum()
             
-        return result      
     
     def is_empty(self) -> bool: 
         return self.current_items.empty
     def get_earliest_expiry_date(self) -> int: 
         return self.current_items["days_till_expiry"].min()
-    
-    def get_total_kcal(self) -> float: 
-        #to dict
-        food_groups_dict = self.fg.food_groups.set_index('type').to_dict(orient='index')
-        #group by 'type' and sum 'servings'
-        grouped_items = self.current_items.groupby('type', as_index=False).agg({'servings': 'sum'})
-        
-        #calc kcals
-        total_kcal = 0
-        for _, row in grouped_items.iterrows():
-            food_type = row['type']
-            servings = row['servings']
-            
-            if food_type in food_groups_dict:
-                food_data = food_groups_dict[food_type]
-                kcal_per_serving = servings * food_data['kg_per_serving'] * food_data['kcal_per_kg']
-                total_kcal += kcal_per_serving
-        
-        return total_kcal
     
     def debug_get_content(self) -> str: 
         """Debugging function to visualized the current content of a location (fridge, pantry)
